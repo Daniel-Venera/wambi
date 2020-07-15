6a8081997abb0a3c17bf6fb1f3d9499bd59cfdc7
Daniel-Venera <daniel.venera.programmer@gmail.com>
2020-07-15 18:19:39 +0200

    menu burger

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 let log = console.log;
 
-log("oui");
-
 let buttonPlayer = document.querySelector(".button-player");
 let iframe = document.querySelector(".youtube-video");
 let closeBtn = document.querySelector(".close-button");
-log(iframe);
+let burgerMenu = document.querySelector(".btn-menu");
+let logo = document.querySelector(".logo");
+let menu = document.querySelector(".menu");
+let menuContent = document.querySelector(".menu-content");
+let menuItems = document.querySelectorAll(".menu-item");
+
 buttonPlayer.addEventListener("click", function () {
   iframe.style.display = "block";
   document.body.style.overflowY = "hidden";
@@ -18,3 +21,41 @@ closeBtn.addEventListener("click", function () {
   closeBtn.style.display = "none";
   iframe.contentWindow.postMessage('{"event":"command","func":"' + "stopVideo" + '","args":""}', "*");
 });
+
+burgerMenu.addEventListener("click", function () {
+  burgerMenu.classList.toggle("open");
+  if (burgerMenu.classList.contains("open")) {
+    menu.style.maxHeight = "100%";
+    logo.setAttribute("src", "./images/logo-inverse.png");
+    setTimeout(function () {
+      menuContent.style.display = "block";
+    }, 300);
+  } else {
+    menu.style.maxHeight = "0";
+    logo.setAttribute("src", "./images/logo.png");
+    menuContent.style.display = "none";
+    closeMenuItems();
+  }
+});
+
+menuItems.forEach(function (e) {
+  e.addEventListener("click", function () {
+    if (e.nextElementSibling.style.display == "block") {
+      e.lastElementChild.style.transform = "rotate(0)";
+      e.nextElementSibling.style.display = "none";
+    } else {
+      closeMenuItems();
+      e.lastElementChild.style.transform = "rotate(180deg)";
+      e.nextElementSibling.style.display = "block";
+    }
+  });
+});
+
+function closeMenuItems() {
+  menuItems.forEach(function (f) {
+    if (f.nextElementSibling.style.display == "block") {
+      f.lastElementChild.style.transform = "rotate(0)";
+      f.nextElementSibling.style.display = "none";
+    }
+  });
+}
