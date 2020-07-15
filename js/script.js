363355ca30cb9fcd2a5a4dc78ae4084937af08ae
Daniel-Venera <daniel.venera.programmer@gmail.com>
2020-07-15 12:22:23 +0200

    stop youtube video after close button

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ let log = console.log;
 log("oui");
 
 let buttonPlayer = document.querySelector(".button-player");
-let iframe = document.querySelector("iframe");
+let iframe = document.querySelector(".youtube-video");
 let closeBtn = document.querySelector(".close-button");
-
+log(iframe);
 buttonPlayer.addEventListener("click", function () {
   iframe.style.display = "block";
   document.body.style.overflowY = "hidden";
@@ -16,4 +16,5 @@ closeBtn.addEventListener("click", function () {
   iframe.style.display = "none";
   document.body.style.overflowY = "visible";
   closeBtn.style.display = "none";
+  iframe.contentWindow.postMessage('{"event":"command","func":"' + "stopVideo" + '","args":""}', "*");
 });
