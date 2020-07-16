92a81b5df1c4e377f0de0ff9870d6954e7637998
Daniel-Venera <daniel.venera.programmer@gmail.com>
2020-07-16 10:58:11 +0200

    search + navbar top desktop + navbar bottom desktop placement

--- js/script.js
+++ js/script.js
@@ -5,26 +5,45 @@ let iframe = document.querySelector(".youtube-video");
 let closeBtn = document.querySelector(".close-button");
 let burgerMenu = document.querySelector(".btn-menu");
 let logo = document.querySelector(".logo");
+let searchBtn = document.querySelector(".btn-search");
+let search = document.querySelector(".search");
+let searchContent = document.querySelector(".search-content");
 let menu = document.querySelector(".menu");
 let menuContent = document.querySelector(".menu-content");
 let menuItems = document.querySelectorAll(".menu-item");
 
-buttonPlayer.addEventListener("click", function () {
-  iframe.style.display = "block";
-  document.body.style.overflowY = "hidden";
-  closeBtn.style.display = "flex";
+searchBtn.addEventListener("click", function () {
+  burgerMenu.classList.toggle("btn-menu-search");
+  if (searchBtn.classList.contains("fa-search")) {
+    search.style.maxHeight = "180px";
+    logo.setAttribute("src", "./images/logo-inverse.png");
+    searchBtn.style.color = "white";
+    searchBtn.classList.remove("fa-search");
+    searchBtn.classList.add("fa-times");
+    setTimeout(function () {
+      searchContent.style.display = "block";
+    }, 300);
+  } else {
+    closeSearch();
+  }
 });
 
-closeBtn.addEventListener("click", function () {
-  iframe.style.display = "none";
-  document.body.style.overflowY = "visible";
-  closeBtn.style.display = "none";
-  iframe.contentWindow.postMessage('{"event":"command","func":"' + "stopVideo" + '","args":""}', "*");
-});
+function closeSearch() {
+  search.style.maxHeight = "0";
+  logo.setAttribute("src", "./images/logo.png");
+  searchBtn.style.color = "var(--purpleDark)";
+  searchBtn.classList.remove("fa-times");
+  searchBtn.classList.add("fa-search");
+  searchContent.style.display = "none";
+}
 
 burgerMenu.addEventListener("click", function () {
   burgerMenu.classList.toggle("open");
   if (burgerMenu.classList.contains("open")) {
+    if (!searchBtn.classList.contains("fa-search")) {
+      burgerMenu.classList.remove("btn-menu-search");
+      closeSearch();
+    }
     menu.style.maxHeight = "100%";
     logo.setAttribute("src", "./images/logo-inverse.png");
     setTimeout(function () {
@@ -59,3 +78,16 @@ function closeMenuItems() {
     }
   });
 }
+
+buttonPlayer.addEventListener("click", function () {
+  iframe.style.display = "block";
+  document.body.style.overflowY = "hidden";
+  closeBtn.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", function () {
+  iframe.style.display = "none";
+  document.body.style.overflowY = "visible";
+  closeBtn.style.display = "none";
+  iframe.contentWindow.postMessage('{"event":"command","func":"' + "stopVideo" + '","args":""}', "*");
+});
